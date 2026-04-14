675206955e0caa1dc619b55ac657ccc1001222af
Diana Komarchuk <dianakocmarchuk2508@gmail.com>
2026-04-14 18:48:22 +0300

    version 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,27 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notif = document.createElement('div');
+
+  notif.classList.add('notification', type);
+
+  const titl = document.createElement('h2');
+  const text = document.createElement('p');
+
+  titl.classList.add('title');
+
+  titl.textContent = title;
+  text.textContent = description;
+
+  notif.setAttribute('style', `top: ${posTop}px; right: ${posRight}px;`);
+
+  notif.append(titl, text);
+
+  document.body.appendChild(notif);
+
+  setTimeout(() => {
+    notif.style.display = 'none';
+  }, 2000);
 };
 
 pushNotification(
